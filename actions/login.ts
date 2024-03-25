bd2504f81319cf882257b3df9888d58d3443c96c
Joao Vitor Oliveira <joaovitorwoliveira@gmail.com>
2024-03-25 17:02:35 -0300

    Pagina de Login e Registro criadas

---
+++ actions/login.ts
@@ -0,0 +1,14 @@
+"use server";
+import * as z from "zod";
+
+import { LoginSchema } from "@/schemas";
+
+export const login = async (values: z.infer<typeof LoginSchema>) => {
+  const validateFields = LoginSchema.safeParse(values);
+
+  if (!validateFields.success) {
+    return { error: "Campos inválidos!" };
+  }
+
+  return { success: "Email enviado!" };
+};
