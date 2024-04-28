641d161ee5e7d034a380a70c68301d828ca7d415
Joao Vitor Oliveira <joaovitorwoliveira@gmail.com>
2024-04-28 18:17:27 -0300

    Adicionando exemplos de Server & Client

--- auth.ts
+++ auth.ts
@@ -63,6 +63,10 @@ export const {
         session.user.role = token.role;
       }
 
+      if (session.user) {
+        session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean;
+      }
+
       return session;
     },
     async jwt({ token }) {
@@ -73,6 +77,8 @@ export const {
       if (!existingUser) return token;
 
       token.role = existingUser.role;
+
+      token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
 
       return token;
     },
