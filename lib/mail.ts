e7617db6d5f5b4d9fe95d7a29722629538157c20
Joao Vitor Oliveira <joaovitorwoliveira@gmail.com>
2024-05-01 10:59:46 -0300

    Deploy: Ajustes 02

--- lib/mail.ts
+++ lib/mail.ts
@@ -6,7 +6,7 @@ const domain = process.env.NEXT_PUBLIC_APP_URL;
 
 export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "mail@joaovitorwoliveira.live",
     to: email,
     subject: "Código de verificação",
     html: `<p>Seu código de verificação é ${token}</p>`,
@@ -17,7 +17,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetLink = `${domain}/auth/new-password?token=${token}`;
 
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "mail@joaovitorwoliveira.live",
     to: email,
     subject: "Redefinir a senha",
     html: `<p>Clique <a href="${resetLink}">aqui</a> para redefinir a senha.</p>`,
@@ -28,7 +28,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `${domain}/auth/new-verification?token=${token}`;
 
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "mail@joaovitorwoliveira.live",
     to: email,
     subject: "Confirme o seu email",
     html: `<p>Clique <a href="${confirmLink}">aqui</a> para confirmar o email.</p>`,
